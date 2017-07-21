47d18a551d7aa087fcac8be2ad9423ff467a49f5
neona <neona@neona-Vostro-3500>
2017-07-21 18:35:34 +0200

    game name as variable

--- pages/home.po.js
+++ pages/home.po.js
@@ -2,16 +2,11 @@
 
 module.exports = {
   locators: {
-    //search_btn: element(by.xpath("//button[@class='btn-search-magnifier']")),
     search_btn: element(by.className("btn-search-magnifier")),
-
     search_input: element(by.xpath("//input[@placeholder='Search for games...']")),
     search_result_title: element(by.xpath("//span[contains(.,'search results')]")),
-    game_block: element(by.xpath("//img[@alt='Mayfair Roulette']")),
     play_btn: element(by.className("o-btn tile__button-play")),
     login_form: element(by.className("modal modal-login")),
-
-
   },
 
   go: function() {
@@ -19,5 +14,12 @@ module.exports = {
     browser.ignoreSynchronization = true;
     var title = browser.getTitle();
     expect(title).toEqual('Play Online Casino Games at William Hill Vegas')
+  },
+
+  get_game_block_by_name: function(name) {
+    var m = element(by.xpath("//img[@alt='" + name + "']"));
+    return m;
+    console.log(m);
   }
+
 }
--- tests/whtest.js
+++ tests/whtest.js
@@ -1,7 +1,7 @@
 var EC = protractor.ExpectedConditions;
 var home_po = require("../pages/home.po.js");
 var locator = home_po.locators;
-
+var game_name = 'Mayfair Roulette'; // enter any Game Name for example: Golden Mane
 describe('WH test suite', function() {
 
   it("Open home page", function() {
@@ -16,15 +16,15 @@ describe('WH test suite', function() {
 
   });
 
-  it("Search for „Mayfair Roulette” game", function() {
+  it("Search fo" + game_name + " game", function() {
     locator.search_input.click();
-    locator.search_input.sendKeys('Mayfair Roulette');
+    locator.search_input.sendKeys(game_name);
     browser.wait(EC.visibilityOf(locator.search_result_title), 10000);
     expect(locator.search_result_title.isDisplayed()).toBe(true);
   });
 
-  it("Hover over", function() {
-    browser.actions().mouseMove(locator.game_block).perform();
+  it("Hover over " + game_name + " game ", function() {
+    browser.actions().mouseMove(home_po.get_game_block_by_name(game_name)).perform();
     browser.wait(EC.visibilityOf(locator.play_btn), 10000);
     expect(locator.play_btn.isDisplayed()).toBe(true);
 
@@ -34,6 +34,6 @@ describe('WH test suite', function() {
     locator.play_btn.click();
     browser.wait(EC.visibilityOf(locator.login_form), 10000);
     expect(locator.login_form.isDisplayed()).toBe(true);
-  })
+  });
 
 });
